cc4cd589ec2aa700e13ad047ae707bfdc436fe2a
hubls <hubls@naver.com>
2016-03-25 00:02:05 +0900

    homework
    homework

---
+++ leesangjun2.py
@@ -0,0 +1,10 @@
+﻿temp=input("user input temperature : ")
+sel=raw_input("F or C : ")
+print temp, sel
+if sel=='F':
+    result=(temp-32)/1.8
+    print result, "C"
+elif(sel=='C'):
+    print result,"C"
+else:
+    print "input Error"
