65110565c13484e53021d7b4c261a9c709e95302
hubls <hubls@naver.com>
2016-03-25 01:11:55 +0900

    Update leesangjun.py

--- leesangjun.py
+++ leesangjun.py
@@ -1,10 +1,11 @@
-﻿temp=input("user input temperature : ")
+temp=input("user input temperature : ")
 sel=raw_input("F or C : ")
 print temp, sel
 if sel=='F':
     result=(temp-32)/1.8
     print result, "C"
-elif(sel=='C'):
-    print result,"C"
+elif sel=='C':
+    result=(temp*1.8+32)
+    print result,"F"
 else:
     print "input Error"
