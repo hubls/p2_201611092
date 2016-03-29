b1b1e36b7fef8c63be4f2d9f41cbb1820b7d2ac5
hubls <hubls@naver.com>
2016-03-29 22:35:48 +0900

    Update leesangjun.py

--- leesangjun.py
+++ leesangjun.py
@@ -1,3 +1,20 @@
+#바람개비 모형 그리기 다이어그램 
+3 %%plantuml 
+4 @startuml
+ 
+5 start 
+6 :set size, bigger, sides, angle; 
+7 repeat 
+8 if (i is multiples of 2) then 
+9     :size=existing size+bigger; 
+10 endif 
+11 :t1.fd(size); 
+12 :t1.right(angle); 
+13 repeat while(sides) 
+14 stop 
+15 @enduml 
+
+#바람개비 모형
 sides=10
 size=10
 bigger=5
