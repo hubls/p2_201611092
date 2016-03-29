0d4ef048f77381aa0a32cd5f92b7df6978f96dbd
hubls <hubls@naver.com>
2016-03-29 22:27:01 +0900

    Update leesangjun.py

--- leesangjun.py
+++ leesangjun.py
@@ -1,11 +1,11 @@
-temp=input("user input temperature : ")
-sel=raw_input("F or C : ")
-print temp, sel
-if sel=='F':
-    result=(temp-32)/1.8
-    print result, "C"
-elif sel=='C':
-    result=(temp*1.8+32)
-    print result,"F"
-else:
-    print "input Error"
+sides=10
+size=10
+bigger=5
+angle=90
+def makeSwirlsquare(size,bigger,sides,angle):
+    for i in range(0,sides):
+        size=size+bigger
+        t1.fd(size)
+        t1.right(angle)
+        
+makeSwirlsquare(10,5,50,90)
