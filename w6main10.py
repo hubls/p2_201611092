6b54bd21baeb2def7fc2ee0ece8816f3fa9d992f
hubls <hubls@naver.com>
2016-04-09 18:44:29 +0900

    Added files via upload

---
+++ w6main10.py
@@ -0,0 +1,31 @@
+﻿def highlowgame():
+    yourtrytime=0 
+    x=input("input minimum") 
+    y=input("input maximum") 
+    USO=input("input user number one") 
+    UST=input("input user number two") 
+
+ 
+    if USO==UST:
+             print "good!" 
+   
+    while UST!=USO: 
+         
+        if UST>=USO: 
+                print "your number is High!" 
+                UST=input("input user number two") 
+                yourtrytime=yourtrytime+1 
+         
+        if UST<=USO: 
+                print "your number is Low!" 
+                UST=input("input user number two") 
+                yourtrytime=yourtrytime+1 
+         
+        if USO==UST: 
+             print "good!" 
+         
+    
+     
+         
+    print "yourtrytime is "+str(yourtrytime)
+highlowgame()
---
+++ w6main9.py
@@ -0,0 +1,10 @@
+﻿year=input("input year: ")
+
+if year%4 == 0 and not year%100 == 0: 
+    result = "yun nyun" 
+elif year%4==0 and year%400==0: 
+    result = "yun nyun" 
+else: 
+    result = "not yun nyun or Input error" 
+     
+print result
