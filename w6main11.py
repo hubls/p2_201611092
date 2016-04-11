6b9668e42dd8a66b5e9e48119a957532e63b9638
hubls <hubls@naver.com>
2016-04-11 18:31:16 +0900

    Added files via upload

---
+++ w6main11.py
@@ -0,0 +1,18 @@
+﻿def sumList(aList): 
+    sum=0 
+    for i in range(0,200): 
+        sum=sum+aList[i] 
+    return sum 
+
+ 
+def lab6(): 
+    aList=list() 
+    for i in range(1,1001): 
+        if i%4==0 and not i%5==0: 
+            aList.append(i) 
+    labsum=sumList(aList) 
+    print labsum 
+     
+def main(): 
+    lab6()
+lab6()
